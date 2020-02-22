7bec3e91fadc68c15bf085106e7b0160bc1a7fc4
Saulius Baltramonaitis <msnfan234@gmail.com>
2020-02-22 23:11:47 +0000

    add endpoint to show number of connections of pilots and controllers

--- API/queries.js
+++ API/queries.js
@@ -1,6 +1,6 @@
 const getData = require("./requestVATSIMData");
 
-const home = (req, res) => {
+const getHome = (req, res) => {
     return res.json({ Info: 'Node.JS, Express', Server: 'Online' });
 }
 
@@ -9,7 +9,32 @@ const getVatsim = async (req, res) => {
     return res.json(data);
 }
 
+const getOnline = async (req, res) => {
+    const data = await getData();
+
+    let output = {};
+    try {
+        let p = 0;
+        for(pilot in data.pilots) {
+            p += 1;
+        }
+
+        let c = 0
+        for(controller in data.controllers) {
+            c += 1;
+        }
+
+        output["pilots"] = p;
+        output["controllers"] = c;
+        res.json(output);
+    }
+    catch(err) {
+        console.log(err);
+    }
+}
+
 module.exports = {
-    home,
-    getVatsim
+    getHome,
+    getVatsim,
+    getOnline
 }
--- API/routes.js
+++ API/routes.js
@@ -2,9 +2,12 @@ const queries = require("./queries")
 
 var appRoutes = (app) => {
     
-    app.get("/", queries.home);
-
+    app.get("/", queries.getHome);
     app.get("/vatsim", queries.getVatsim);
+    app.get("/vatsim/online", queries.getOnline);
+    //app.get("/vatsim/voice", queries.byVoice);
+    //app.get("/vatsim/receive", queries.byReceive);
+    //app.get("/vatsim/text", queries.byText);
 }
 
 module.exports = appRoutes;
