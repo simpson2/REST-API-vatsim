79f22bb6b6bbade4ab61600d6ce075afdbb927b9
Saulius Baltramonaitis <msnfan234@gmail.com>
2020-03-13 22:39:02 +0000

    add id for each pilot returned with remark-search

--- functions/remarkSearch.js
+++ functions/remarkSearch.js
@@ -2,13 +2,15 @@ function remarkSearch(pilots, remarkParam) {
     
     const pattern = new RegExp("^.*" + remarkParam + ".*$", "i");
     const pilotAndRemarks = {};
+    let id = 0;
 
     for(i = 0; i < pilots.length; i++) {
         if(pattern.test([pilots[i].plan.remarks])) {
+            id++;
 
-            pilotAndRemarks["Pilot Name "+i] = pilots[i].member.name;
-            pilotAndRemarks["Pilot CID "+i] = pilots[i].member.cid;
-            pilotAndRemarks["Remarks "+i] = pilots[i].plan.remarks;
+            pilotAndRemarks["Pilot Name "+id] = pilots[i].member.name;
+            pilotAndRemarks["Pilot CID "+id] = pilots[i].member.cid;
+            pilotAndRemarks["Remarks "+id] = pilots[i].plan.remarks;
         }
     }
 
