fd9622002f54054933eac240a1f32cf166a9f30f
Saul <msnfan234@gmail.com>
2024-09-13 21:23:10 +0100

    update regex expression to remove unnecessary parameters

--- functions/determineVoiceStatus.js
+++ functions/determineVoiceStatus.js
@@ -1,6 +1,6 @@
-const voiceRegex = /^.*\/(v|V)\/.*$/;
-const receiveRegex = /^.*\/(r|R)\/.*$/;
-const textRegex = /^.*\/(t|T)\/.*$/;
+const voiceRegex = /\/(v|V)\//;
+const receiveRegex = /\/(r|R)\//;
+const textRegex = /\/(t|T)\//;
 
 function determineVoiceStatus(remarks) {
 
@@ -9,7 +9,7 @@ function determineVoiceStatus(remarks) {
     let t = 0;
     let u = 0;
     let output = {};
-    
+
     for(i = 0; i < remarks.length; i++) {
         if(voiceRegex.test(remarks[i])) v += 1;
         else if(receiveRegex.test(remarks[i])) r += 1;
